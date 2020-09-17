ab98262a7270b6d82f9b7c0b8bb7f6bef14a90f6
Charles Marshall <csmarshall@users.noreply.github.com>
2020-09-17 15:28:03 -0500

    Updates: Updating Readme Tweaks to timings and graph info Changed name to 'surfboard' to refer to the product line

--- README.md
+++ README.md
@@ -1,18 +1,67 @@
-# netdata-arris-sb8200-modem-plugin-python
-netdata python.d collector for Arris Surfboard 8200 Modem downstream/upstream stats
+# netdata-arris-surfboard-modem-plugin-python
+netdata python.d collector for Arris Surfboard Modem downstream/upstream stats
+
+## To install:
+This script scrapes the private 192.168.100.1 cmconnectionstatus.html page exposed on your cable modem, if you should need to do any configuration more than simply copying (or symlinking if your OS supports it) the script into place
+1. Checkout the repo:
+```zsh
+user@netdata:~ % git clone git@github.com:csmarshall/netdata-arris-surfboard-modem-plugin-python.git
+Cloning into 'netdata-arris-surfboard-modem-plugin-python'...
+remote: Enumerating objects: 13, done.
+remote: Counting objects: 100% (13/13), done.
+remote: Compressing objects: 100% (12/12), done.
+remote: Total 13 (delta 3), reused 8 (delta 0), pack-reused 0
+Receiving objects: 100% (13/13), 5.63 KiB | 5.63 MiB/s, done.
+Resolving deltas: 100% (3/3), done.
+```
+2. cd to you're netdata base install dir, This is where you installed netdata, in this example: /usr/www/netdata, but by default this *may* be "/"
+```zsh
+user@netdata:~ % cd /usr/www/netdata
+```
+
+3. Symlink the charts script from your checkout from the checked out repo *into* your netdata install:
+```zsh
+user@netdata:/usr/www/netdata % sudo ln -s ~/netdata-arris-surfboard-modem-plugin-python/arris_surfboard_modem.chart.py ./usr/libexec/netdata/python.d/arris_surfboard_modem.chart.py
+user@netdata:/usr/www/netdata % ls -la ./usr/libexec/netdata/python.d/arris_surfboard_modem.chart.py
+lrwxr-xr-x 1 root daemon 78 Sep 17 15:05 ./usr/libexec/netdata/python.d/arris_surfboard_modem.chart.py -> /home/user/netdata-arris-surfboard-modem-plugin-python/arris_surfboard_modem.chart.py
+```
+
+4. OPTIONAL?: Symlink the stub config into place:
+```zsh
+user@netdata:/usr/www/netdata % sudo ln -sf ~/work/netdata-arris-surfboard-modem-plugin-python/arris_surfboard_modem.conf ./usr/lib/netdata/conf.d/python.d/arris_surfboard_modem.conf
+user@netdata:/usr/www/netdata % ls -la ./usr/lib/netdata/conf.d/python.d/arris_surfboard_modem.conf
+lrwxr-xr-x 1 root netdata 86 Sep 17 15:11 ./usr/lib/netdata/conf.d/python.d/arris_surfboard_modem.conf -> /home/user/work/netdata-arris-surfboard-modem-plugin-python/arris_surfboard_modem.conf
+```
+Then edit the config if you so choose:
+```zsh
+user@netdata:/usr/www/netdata % cd etc/netdata
+user@netdata:/usr/www/netdata/etc/netdata %  sudo ./edit-config python.d/arris_surfboard_modem.conf
+```
+
+5. Restart netdata, obviously this is OS specific:
+```zsh
+user@netdata:/usr/www/netdata % sudo service netdata restart
+netdata not running? (check /usr/www/netdata/var/run/netdata/netdata.pid).
+Starting netdata.
+2020-09-17 13:34:07: netdata INFO  : MAIN : SIGNAL: Not enabling reaper
+user@netdata:/usr/www/netdata %
+```
+
+6. Profit 😎
 
 ## Notes:
+* This is for the [Arris Surfboard Family]|(https://www.arris.com/surfboard/products/cable-modems/) family of DOCSIS Cable Modems, I tested with the [SB8200]|(https://www.arris.com/surfboard/products/cable-modems/sb8200/) but I have a loose memory of this similar UI existing in previous generations, and possibly even legacy Motorola surfboard devices, though there might need to be some slight modifications due to HTML changes.
+* If you're looking for other Arris cable modem products you may want to look at [@theY4Kman's plugin]|(https://github.com/theY4Kman/netdata-arris-modem-plugin-python), it was the starting point for this script, and hopefully should work if your modem exposes a "cgi-bin" style status page.
 * My modem seems to have a roughly 9~ second delay when querying these stats:
 ```zsh
 % for A in {1..100}; do echo "${A},$(curl -w '%{time_total}\n' -o /dev/null -s http://192.168.100.1/cmconnectionstatus.html)" ; done | recs fromcsv -k run,time | recs collate -a p50_time=perc,50,time
 {"p50_time":"8.797505"}
 ```
-As a result, I've increase the default interval to 20 seconds to be safe, you may want to tweak it, but for me this seems like a happy medium, as far as I know there's no impact to the dataplane of the modem from querying these stats so frequently.
-* I only have a Surfboard 8200, but possibly any Surfboard modem that supports the "cmconnectionstatus.html" page may work.
-* I can't thank @theY4Kman enough for the initial framework, [his plugin]|(https://github.com/theY4Kman/netdata-arris-modem-plugin-python) should hopefully work if your modem exposes a "cgi-bin" style status page.
+As a result, I've increase the default interval to 20 seconds to be safe, with a 15 second timeout, you may want to tweak it, but for me this seems like a happy medium, as far as I know there's no impact to the dataplane of the modem from querying these stats so frequently.
 
 ## Todo:
-* Fix the graphs...
-* Instructions to install
-* Change the default icon?
-* See if there's a way to reduce the interval?
+* ~~Instructions to install~~
+* Possibly submit this upstream?
+* Add graph screenshots
+* Change the default icon in the UI?
+* See if there's a way to reduce the interval.
--- arris_surfboard_modem.chart.py
+++ arris_surfboard_modem.chart.py
@@ -6,63 +6,59 @@ from bases.FrameworkServices.UrlService import UrlService
 
 priority = 90000
 
-ORDER = []
-CHARTS = {}
-
-for direction in ['down', 'up']:
-    for graph in ['frequency', 'power', 'signal_to_noise', 'corrected_errors', 'uncorrected_errors']:
-        ORDER.append(f"{direction}stream_{graph}")
-
 CHARTS = {
     'downstream_frequency': {
-        'options': [None, 'Frequency', 'mHz', 'arris_sb8200', 'arris_sb8200.downstream_frequency', 'line'],
+        'options': [None, 'Frequency', 'mHz', 'Downstream', 'arris_surfboard.downstream_frequency', 'line'],
         'lines': [
-            ['downstream_{n}_frequency', '{n}', 'absolute', 1, 1000],
+            ['downstream_{n}_frequency', '{n}', 'absolute'],
         ],
     },
     'downstream_power': {
-        'options': [None, 'Power', 'dBmV', 'arris_sb8200', 'arris_sb8200.downstream_power', 'line'],
+        'options': [None, 'Power', 'dBmV', 'Downstream', 'arris_surfboard.downstream_power', 'line'],
         'lines': [
-            ['downstream_{n}_power', '{n}', 'absolute', 1, 100],
+            ['downstream_{n}_power', '{n}', 'absolute'],
         ],
     },
     'downstream_signal_to_noise': {
-        'options': [None, 'Signal to Noise', 'dB', 'arris_sb8200', 'arris_sb8200.downstream_signal_to_noise', 'line'],
+        'options': [None, 'Signal to Noise', 'dB', 'Downstream', 'arris_surfboard.downstream_signal_to_noise', 'line'],
         'lines': [
-            ['downstream_{n}_signal_to_noise', '{n}', 'absolute', 1, 100],
+            ['downstream_{n}_signal_to_noise', '{n}', 'absolute'],
         ],
     },
     'downstream_corrected_errors': {
-        'options': [None, 'Corrected Errors', 'correcteds', 'arris_sb8200', 'arris_sb8200.downstream_corrected_errors', 'line'],
+        'options': [None, 'Corrected Errors', 'correcteds', 'Downstream', 'arris_surfboard.downstream_corrected_errors', 'line'],
         'lines': [
             ['downstream_{n}_corrected_errors', '{n}', 'incremental'],
         ],
     },
     'downstream_uncorrected_errors': {
-        'options': [None, 'Uncorrected Errors', 'uncorrectables', 'arris_sb8200', 'arris_sb8200.downstream_uncorrected_errors', 'line'],
+        'options': [None, 'Uncorrected Errors', 'uncorrectables', 'Downstream', 'arris_surfboard.downstream_uncorrected_errors', 'line'],
         'lines': [
             ['downstream_{n}_uncorrected_errors', '{n}', 'incremental'],
         ],
     },
     'upstream_frequency': {
-        'options': [None, 'Frequency', 'mHz', 'arris_sb8200', 'arris_sb8200.upstream_frequency', 'line'],
+        'options': [None, 'Frequency', 'mHz', 'Upstream', 'arris_surfboard.upstream_frequency', 'line'],
         'lines': [
-            ['upstream_{n}_frequency', '{n}', 'absolute', 1, 1000],
+            ['upstream_{n}_frequency', '{n}', 'absolute'],
         ],
     },
     'upstream_width': {
-        'options': [None, 'Width', 'mHz', 'arris_sb8200', 'arris_sb8200.upstream_width', 'line'],
+        'options': [None, 'Width', 'mHz', 'Upstream', 'arris_surfboard.upstream_width', 'line'],
         'lines': [
-            ['upstream_{n}_width', '{n}', 'absolute', 1, 100],
+            ['upstream_{n}_width', '{n}', 'absolute'],
         ],
     },
     'upstream_power': {
-        'options': [None, 'Power', 'dBmV', 'arris_sb8200', 'arris_sb8200.upstream_power', 'line'],
+        'options': [None, 'Power', 'dBmV', 'Upstream', 'arris_surfboard.upstream_power', 'line'],
         'lines': [
-            ['upstream_{n}_power', '{n}', 'absolute', 1, 100],
+            ['upstream_{n}_power', '{n}', 'absolute'],
         ],
     }
 }
+
+# Why would we want an order different than what we already defined 🤷 ?
+ORDER = [i for i in CHARTS]
 
 html_parser = etree.HTMLParser()
 
@@ -73,14 +69,19 @@ class Service(UrlService):
             configuration = {}
         configuration.setdefault('url', 'http://192.168.100.1/cmconnectionstatus.html')
 
+        # Out of 100 curls the p50 the total_time metric was 8.7 seconds, so 20
+        # seconds is frequent enough to give useful data, but not so much so
+        # that it takes *too* long.
+        configuration.setdefault('update_every', 20)
+
         super(Service, self).__init__(configuration=configuration, name=name)
         # My modem's status page takes several seconds to load, does yours?
-        self.request_timeout = 20
+        self.request_timeout = 15
 
         self.order = ORDER
         self.definitions = deepcopy(CHARTS)
 
-    def create_definitions(self, num_streams):
+    def create_definitions(self, num_streams=0):
         self.debug(self.definitions)
         for chart in self.definitions.values():
             lines = chart['lines']
@@ -99,6 +100,18 @@ class Service(UrlService):
 
             stream_rows = {'down': {}, 'up': {}}
 
+# Channel info dict of dicts:
+#           ┌────────────┐
+#           │   Direct   │
+#           └────────────┘
+#                  │  ┌────────────┐
+#                  └─▶│  Channel   │
+#                     └────────────┘
+#                            │  ┌────────────┐
+#                            └─▶│   Metric   │
+#                               └────────────┘
+
+            # Evaluate the "downstrem" then "upstream" channel info
             for direction in ['down', 'up']:
                 tbls = root.xpath('//table')
                 for tbl in tbls:
@@ -107,14 +120,15 @@ class Service(UrlService):
 
                         table_rows = tbl.getchildren()
                         for table_row in table_rows:
-                            # first row has only the "Downstream ..." th
-                            # second row has "Channel" header
+                            # first row has only the "<direction>stream ..." th
+                            # second starts with "Channel" header
                             tds = table_row.xpath('./td')
                             if len(tds) == 0 or tds[0].text.startswith("Channel"):
                                 self.debug(f'Skipping Header line: "{tds}"')
                                 continue
 
                             vals = [col.text.encode('UTF-8').decode().strip() for col in tds]
+                            # Upstream has 7 values, Downstream has 8
                             if len(vals) < 7 or len(vals) > 8:
                                 self.debug(f"Skipping: Not enough, or too many columns {len(vals)}")
                                 continue
@@ -122,26 +136,35 @@ class Service(UrlService):
                             if direction == "down":
                                 channel = table_row[0].text
                                 channel_data = {
-                                    'frequency': float(table_row[3].text.split(' ', 1)[0]) / 1000000,
-                                    'power': float(table_row[4].text.split(' ', 1)[0]) * 10,
-                                    'signal_to_noise': float(table_row[5].text.split(' ', 1)[0]),
-                                    'corrected_errors': int(table_row[6].text),
-                                    'uncorrected_errors': int(table_row[7].text),
+                                    'frequency': float(table_row[3].text.split(' ', 1)[0]) / 1000000,  # mHz
+                                    'power': float(table_row[4].text.split(' ', 1)[0]) * 10,  # dBmV
+                                    'signal_to_noise': float(table_row[5].text.split(' ', 1)[0]),  # dB
+                                    'corrected_errors': int(table_row[6].text),  # int
+                                    'uncorrected_errors': int(table_row[7].text),  # int
                                 }
                             elif direction == "up":
+
+                                # On the 8200 there is a channel "#" at col 0
+                                # and a channel "id" at col 1, I picked channel
+                                # id, since that seems more useful.
                                 channel = table_row[1].text
                                 channel_data = {
-                                    'frequency': float(table_row[4].text.split(' ', 1)[0]) / 1000000,
-                                    'width': float(table_row[5].text.split(' ', 1)[0]) / 1000000,
-                                    'power': float(table_row[6].text.split(' ', 1)[0]),
+                                    'frequency': float(table_row[4].text.split(' ', 1)[0]) / 1000000,  # mHz
+                                    'width': float(table_row[5].text.split(' ', 1)[0]) / 1000000,  # mHz
+                                    'power': float(table_row[6].text.split(' ', 1)[0]),  # dBmV
                                 }
-                            self.debug(f"{direction} channel: {channel} channel_data: {channel_data}")
+                            self.debug(f"{direction}stream channel: {channel} channel_data: {channel_data}")
                             stream_rows[direction][channel] = channel_data
 
             self.debug(stream_rows)
             return stream_rows
+
         except (ValueError, AttributeError):
-            self.debug("Unable to parse {direction}stream rows")
+            self.debug("Unable to parse html rows")
+            return ()
+
+        except Exception as ex:
+            self.debug(f"Unhandled exeption: {ex}")
             return ()
 
     def _get_data(self):
@@ -150,9 +173,22 @@ class Service(UrlService):
         for direction in ["down", "up"]:
             self.debug(f"Direction: {direction}")
             if direction == "down":
-                channel_metrics = ['frequency', 'power', 'signal_to_noise', 'corrected_errors', 'uncorrected_errors']
+                channel_metrics = [
+                    'frequency',
+                    'power',
+                    'signal_to_noise',
+                    'corrected_errors',
+                    'uncorrected_errors'
+                ]
             elif direction == "up":
-                channel_metrics = ['frequency', 'width', 'power']
+                channel_metrics = [
+                    'frequency',
+                    'width',
+                    'power'
+                ]
+
+            # We're going to return a consistently sorted list, of the channels
+            # in numerical order,
             for channel in sorted(stream_rows[direction]):
                 for key in channel_metrics:
                     return_data[f'{direction}stream_{channel}_{key}'] = stream_rows[direction][channel][key]
@@ -168,7 +204,10 @@ class Service(UrlService):
             return False
 
         try:
+            # Parse data from the URL
             data = self._get_data()
+            # I create the deffinitions after pulling and parsing the data to
+            # avoid having to query the UI twice.
             self.create_definitions(len(data))
 
         except Exception as error:
